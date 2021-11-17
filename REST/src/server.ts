c337b76c2fda47b700471489d0859938948f7673
Miyukiichan <sam.matthews19638@gmail.com>
2021-11-17 22:16:24 +0000

    - Generic update DB function - Add users table and endpoint - General tidying up

--- REST/src/server.ts
+++ REST/src/server.ts
@@ -14,7 +14,11 @@ var app = express();
 
 app.use(express.json());
 
-function queryDB(query: string, values: any[], successFunc: Function): void {
+function queryDB(
+  query: string,
+  values: any[] = [],
+  successFunc: Function = () => {}
+): void {
   pool.connect((err, client, done) => {
     if (err) {
       throw err;
@@ -48,13 +52,47 @@ interface UpdateField {
   value?: any;
 }
 
+class UserInputError {
+  message: string;
+  constructor(message) {
+    this.message = message;
+  }
+}
+
 function Update(
   table: string,
   fields: UpdateField[],
-  filters: string[],
+  filter: string,
+  filterValues: any[],
   body,
   successFunc: Function
-): void {}
+): void {
+  var query = `update ${table} set `;
+  var values = [];
+  for (var i = 0; i < fields.length; i++) {
+    var { name, value } = fields[i];
+    if (!name) throw "No field name provided in update statement";
+    if (!value) value = body[name];
+    if (value) {
+      values.push(value);
+      if (values.length > 1) query += ", ";
+      query += `${name} = $${values.length}`;
+    }
+  }
+  if (values.length == 0)
+    throw new UserInputError("No values passed to update");
+  var splitFilter = filter.split("$");
+  if (splitFilter.length > 0) {
+    i = values.length;
+    for (var spl of splitFilter) {
+      if (spl == "") continue;
+      i++;
+      query += ` ${spl}$${i}`;
+    }
+    values = values.concat(filterValues);
+  }
+  queryDB(query, values, successFunc);
+}
 
 function Insert(
   table: string,
@@ -70,10 +108,10 @@ function Insert(
     if (!value) value = body[name];
     if (value) {
       values.push(value);
-      if (i != 0) query += ", ";
+      if (values.length > 1) query += ", ";
       query += name;
     } else if (required) {
-      throw `Required field ${name} not provided!`;
+      throw new UserInputError(`Required field ${name} not provided!`);
     }
   }
   if (values.length == 0) throw "Nothing to insert!";
@@ -86,58 +124,150 @@ function Insert(
   queryDB(query, values, successFunc);
 }
 
+function catchUserError(res, queryFunc: Function) {
+  try {
+    queryFunc();
+  } catch (e) {
+    if (e instanceof UserInputError) {
+      res.status(400).send(e.message);
+    } else throw e;
+  }
+}
+
+const TODO = "todo";
+
+const ID = "id";
+const SHORT_DESC = "short_desc";
+const USER_ID = "user_id";
+const LONG_DESC = "long_desc";
+const DATE_CREATED = "date_created";
+const DATE_COMPLETED = "date_completed";
+const DUE_DATE = "due_date";
+
+const USERS = "users";
+const FIRST_NAME = "first_name";
+const LAST_NAME = "last_name";
+const EMAIL = "email";
+
 queryDB(
-  "create table if not exists todo (id serial primary key,\
-  short_desc VARCHAR(255),\
-  long_desc VARCHAR(1000),\
-  date_created timestamp,\
-  date_completed timestamp,\
-  due_date timestamp)",
-  [],
-  () => {}
+  `CREATE TABLE IF NOT EXISTS ${USERS} ( \
+    ${ID} SERIAL PRIMARY KEY, \
+    ${FIRST_NAME} VARCHAR(255), \
+    ${LAST_NAME} VARCHAR(255), \
+    ${EMAIL} VARCHAR(255), \
+    ${DATE_CREATED} TIMESTAMP \
+    )`
 );
 
-//All Items
+queryDB(
+  `CREATE TABLE IF NOT EXISTS ${TODO} ( \
+    ${ID} SERIAL PRIMARY KEY, \
+    ${USER_ID} INT, \
+    ${SHORT_DESC} VARCHAR(255), \
+    ${LONG_DESC} VARCHAR(1000), \
+    ${DATE_CREATED} TIMESTAMP, \
+    ${DATE_COMPLETED} TIMESTAMP, \
+    ${DUE_DATE} TIMESTAMP, \
+    CONSTRAINT fk_user \
+      FOREIGN KEY(USER_ID) \
+      REFERENCES USERS(ID) \
+    )`
+);
+
+//TODO TABLE
 app.get("/items", (req, res) => {
-  const query = "select * from todo";
+  const query = `select * from ${TODO}`;
   queryAll(query, null, res);
 });
 
-//Per ID
 app.get("/items/:id", (req, res) => {
   var values = [req.params.id];
-  const query = "select * from todo where id = $1";
+  const query = `select * from ${TODO} where ${ID} = $1`;
   queryAll(query, values, res);
 });
 
 app.post("/items", (req, res) => {
-  Insert(
-    "todo",
-    [
-      { name: "short_desc", required: true },
-      { name: "date_created", value: new Date().toISOString() },
-      { name: "long_desc" },
-      { name: "due_date" },
-    ],
-    req.body,
-    () => res.status(200).send("Successfully added " + req.body.short_desc)
-  );
+  catchUserError(res, () => {
+    Insert(
+      TODO,
+      [
+        { name: SHORT_DESC, required: true },
+        { name: USER_ID, required: true },
+        { name: DATE_CREATED, value: new Date().toISOString() },
+        { name: LONG_DESC },
+        { name: DUE_DATE },
+      ],
+      req.body,
+      () =>
+        res
+          .status(200)
+          .send(`Successfully added todo item ${req.body.short_desc}`)
+    );
+  });
 });
 
 app.patch("/items/:id", (req, res) => {
-  const { id } = req.params;
-  Update(
-    "todo",
-    [
-      { name: "short_desc" },
-      { name: "date_created" },
-      { name: "long_desc" },
-      { name: "due_date" },
-    ],
-    [`id = ${id}`],
-    req.body,
-    () => res.status(200).send(`Successfully updated item id ${id}`)
-  );
+  catchUserError(res, () => {
+    const { id } = req.params;
+    Update(
+      TODO,
+      [
+        { name: SHORT_DESC },
+        { name: DATE_COMPLETED },
+        { name: LONG_DESC },
+        { name: DUE_DATE },
+      ],
+      `where ${ID} = $`,
+      [id],
+      req.body,
+      () => res.status(200).send(`Successfully updated item with id ${id}`)
+    );
+  });
+});
+
+//USER TABLE
+app.get("/users", (req, res) => {
+  const query = `select * from ${USERS}`;
+  queryAll(query, null, res);
+});
+
+app.get("/users/:id", (req, res) => {
+  var values = [req.params.id];
+  const query = `select * from ${USERS} where ${ID} = $1`;
+  queryAll(query, values, res);
+});
+
+app.post("/users", (req, res) => {
+  catchUserError(res, () => {
+    Insert(
+      USERS,
+      [
+        { name: EMAIL, required: true },
+        { name: DATE_CREATED, value: new Date().toISOString() },
+        { name: FIRST_NAME },
+        { name: LAST_NAME },
+      ],
+      req.body,
+      () =>
+        res
+          .status(200)
+          .send(`Successfully added user with email ${req.body.email}`)
+    );
+  });
+});
+
+app.patch("/users/:id", (req, res) => {
+  catchUserError(res, () => {
+    const { id } = req.params;
+    Update(
+      USERS,
+      [{ name: EMAIL }, { name: FIRST_NAME }, { name: LAST_NAME }],
+      `where ${ID} = $`,
+      [id],
+      req.body,
+      () => res.status(200).send(`Successfully updated user with id ${id}`)
+    );
+  });
 });
 
 //Listen
